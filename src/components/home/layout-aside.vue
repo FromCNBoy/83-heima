f0cf44f4e07f5fd6054b25dd90818bf887a5ae1e
Your Name <1586697534@qq.com>
2019-09-19 15:08:19 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="layout-aside">
+    <div class="title">
+      <img src="../../assets/img/logo_admin.png" alt />
+    </div>
+    <el-menu
+      style="width:201px"
+      background-color="#353b4e"
+      text-color="#adafb5"
+      active-text-color="#ffd04b">
+      <el-menu-item index="4">
+        <i class="el-icon-s-home"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-document"></i>
+          <span>内容管理</span>
+        </template>
+          <el-menu-item index="1-1">发布文章</el-menu-item>
+          <el-menu-item index="1-2">内容列表</el-menu-item>
+          <el-menu-item index="1-3">评论列表</el-menu-item>
+          <el-menu-item index="1-4">素材管理</el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-s-custom"></i>
+          <span>粉丝管理</span>
+        </template>
+          <el-menu-item index="2-1">图文数据</el-menu-item>
+          <el-menu-item index="2-2">粉丝概况</el-menu-item>
+          <el-menu-item index="2-3">粉丝画像</el-menu-item>
+          <el-menu-item index="2-4">粉丝列表</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-s-tools"></i>
+        <span slot="title">账户信息</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.layout-aside {
+  background-color: #323745;
+  width: 200px;
+  height: 100vh;
+  .title {
+    text-align: center;
+    background-color: #2e2f32;
+    padding: 15px 0;
+    img {
+      height: 30px;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>我是主页</div>
+  <el-container>
+    <!-- 左侧导航 -->
+    <el-aside>
+      <layout-aside>导航</layout-aside>
+    </el-aside>
+    <!-- 内容区域 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>头部</el-header>
+      <el-main>
+        <!-- 路由容器 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
