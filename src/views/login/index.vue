a4186ff8b879a459b44a5b6f6edbd62e1c6e4714
Your Name <1586697534@qq.com>
2019-09-19 09:54:36 +0800

    ## 黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,20 +4,23 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
-      <el-form style="maegin-top:20px">
-        <el-form-item>
-          <el-input></el-input>
+      <el-form ref="myForm" :model="loginForm" :rules="loginRules" style="maegin-top:20px">
+        <el-form-item prop="mobile">
+          <!-- 输入手机号 -->
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:65%"></el-input>
+        <el-form-item prop="code">
+             <!-- 发送/接收验证码 -->
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:65%"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="agree">
+           <!-- 同意选项 -->
+          <el-checkbox  v-model="loginForm.agree" >我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
           <!-- 登录按钮 -->
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button @click="login" type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -25,7 +28,44 @@
 </template>
 
 <script>
-export default {}
+
+export default {
+  data () {
+    let validator = function (rule, value, callBack) {
+      //   if (value) {
+      //     callBack()
+      //   } else {
+      //     callBack(new Error('请您阅读用户协议和隐私条款'))
+      //   }
+      value ? callBack() : callBack(new Error('请您阅读用户协议和隐私条款'))
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        agree: false
+      },
+      loginRules: {
+
+        mobile: [{ required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }],
+        code: [{ required: true, message: '请输入您的验证码' },
+          { pattern: /^\d{6}$/, message: '请输入正确的验证码' }],
+        agree: [{ validator }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 校验整个表单的规则
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          console.log('校验成功')
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
