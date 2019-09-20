237a10cd93695d5d4e82ce51728b72763f3c0551
Your Name <1586697534@qq.com>
2019-09-20 21:29:24 +0800

    黑马头条PC-主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -6,15 +6,15 @@
     </el-col>
     <el-col :span="4">
       <img class="head-img" :src="! userInfo.photo ? userInfo.photo : defaultImg" alt />
-      <el-dropdown trigger="click">
+      <el-dropdown trigger="click" @command="commonClick">
         <span class="el-dropdown-link">
           {{userInfo.name}}
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item icon="el-icon-plus">个人信息</el-dropdown-item>
-          <el-dropdown-item icon="el-icon-circle-plus">git地址</el-dropdown-item>
-          <el-dropdown-item icon="el-icon-circle-plus-outline">退出</el-dropdown-item>
+          <el-dropdown-item command="accout">个人信息</el-dropdown-item>
+          <el-dropdown-item command="git">git地址</el-dropdown-item>
+          <el-dropdown-item command="lgout">退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -38,6 +38,15 @@ export default {
       }).then(result => {
         this.userInfo = result.data.data
       })
+    },
+    commonClick (key) {
+      if (key === 'accout') {
+      } else if (key === 'git') {
+        window.location.href = 'https://github.com/shuiruohanyu/83heimatoutiao'
+      } else {
+        window.localStorage.clear()
+        this.$router.push('/login')
+      }
     }
   },
   created () {
