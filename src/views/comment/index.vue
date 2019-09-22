c51a0e63cb86ccfe969bafa08137e13727d2001b
Your Name <1586697534@qq.com>
2019-09-22 20:09:06 +0800

    黑马头条PC-评论列表-列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -27,6 +27,11 @@
         </template>
       </el-table-column>
     </el-table>
+     <!-- 分页页面结构 -->
+    <el-row type="flex" justify="center" style='margin:20px 0'>
+      <!-- page-size 是每页多少条 -->
+      <el-pagination  @current-change="changePage" :current-page="page.currentPage" :page-size="page.pageSize" :total="page.total" background layout="prev, pager, next" ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -34,17 +39,27 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        total: 0, // 总条数
+        currentPage: 1, // 默认第一页
+        pageSize: 10 // 每页多少条
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage // 更新最新页码给 currernpage
+      this.getComment()
+    },
     //   获取评论列表
     getComment () {
       this.$axios({
         url: '/articles',
-        params: { response_type: 'comment' } // params 是路径参数也就是 query参数
+        params: { response_type: 'comment', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results // 把返回的数据赋值给list
+        this.page.total = result.data.total_count // 把总条数给 分页组件的总条数
       })
     },
     stateFormatter (row, column, cellValue, index) {
@@ -53,7 +68,7 @@ export default {
     // 打开或者关闭  row 当前行数据
     closeOrOpen (row) {
       let mess = row.comment_status ? '关闭' : '打开'
-      this.$confirm(`您确定要${mess}评论?`).then(() => {
+      this.$confrm(`您确定要${mess}评论?`).then(() => {
         // 调用接口
         this.$axios({
           url: 'comments/status',
